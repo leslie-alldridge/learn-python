0b83f96236d0a4ada030383d905ce60b704de1f7
leslie-alldridge <leslie.alldridge@gmail.com>
2018-11-02 22:18:37 +1300

    strings exercise

--- Basics/helloworld.py
+++ Basics/helloworld.py
@@ -49,3 +49,60 @@ if x_list.count(x) == 10 and y_list.count(y) == 10:
     print("Almost there...")
 if big_list.count(x) == 10 and big_list.count(y) == 10:
     print("Great!")
+
+# formatting strings
+# Here are some basic argument specifiers you should know:
+
+# %s - String (or any object with a string representation, like numbers)
+
+# %d - Integers
+
+# %f - Floating point numbers
+
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+# %x/%X - Integers in hex representation (lowercase/uppercase)
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)
+
+##############################
+# more strings!
+##############################
+
+s = "Strings are awesome!"
+# Length should be 20
+print("Length of s = %d" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5])  # Start to 5
+print("The next five characters are '%s'" % s[5:10])  # 5 to 10
+print("The thirteenth character is '%s'" % s[12])  # Just number 12
+print("The characters with odd index are '%s'" % s[1::2])  # (0-based indexing)
+print("The last five characters are '%s'" % s[-5:])  # 5th-from-last to end
+
+# Convert everything to uppercase
+print("String in uppercase: %s" % s.upper())
+
+# Convert everything to lowercase
+print("String in lowercase: %s" % s.lower())
+
+# Check how a string starts
+if s.startswith("Str"):
+    print("String starts with 'Str'. Good!")
+
+# Check how a string ends
+if s.endswith("ome!"):
+    print("String ends with 'ome!'. Good!")
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
