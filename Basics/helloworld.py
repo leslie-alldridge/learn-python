8ac30f52a88ffdfeb01e00657eace893e6f81468
leslie-alldridge <leslie.alldridge@gmail.com>
2018-11-02 22:43:05 +1300

    classes

--- Basics/helloworld.py
+++ Basics/helloworld.py
@@ -176,3 +176,45 @@ def name_the_benefits_of_functions():
 
 
 name_the_benefits_of_functions()
+
+#########
+# Objects
+#########
+
+# define the Vehicle class
+
+
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (
+            self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+
+
+car1 = Vehicle()
+# name="Fer", kind="convertible", color="red", value="$60,000.00"
+car1.name = "Fer"
+car1.kind = "convertible"
+car1.color = "red"
+car1.value = 60000.00
+
+# car2
+car2 = Vehicle()
+car2.name = "Jump"
+car2.kind = "van"
+car2.color = "blue"
+car2.value = 10000.00
+
+# test code
+print(car1.description())
+print(car2.description())
+
+###############
+# Dictionaries
+###############
